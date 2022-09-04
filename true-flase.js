934de55a2241bda3f3077a69b7234d07ba48826a
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-05 01:28:00 +0600

    js tricky

---
+++ true-flase.js
@@ -0,0 +1,20 @@
+// PH 35-3 Different Truthy and Falsy values in JavaScript
+
+// *truthy
+// any number is positive or negetive is Truthy 
+// any string including single whitespace is truthy 
+
+
+// !falsy 
+// 0 zero is falsy value
+// empty string is falsy 
+
+
+
+const x = false;
+
+if(x){
+    console.log('truthy')
+}else {
+    console.log('false')
+}
