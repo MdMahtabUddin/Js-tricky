9937b897242ca9794164be3c6a332e64e013cafc
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-05 01:51:17 +0600

    undefined &null

---
+++ undefined-null.js
@@ -0,0 +1,14 @@
+// PH 35-4 Null Vs Undefined, different ways you will get undefined
+
+// undefined  
+// varriable value not assign 
+// function didn't return anything 
+// just wrote return but did't return anything 
+// parameter isn't passes 
+// property that doesn't exits is objects 
+// accessing array Element out of range 
+// accessing array delete Element
+
+
+// null 
+// null mane nai , undefined manei kono karone deya hoi nai 
