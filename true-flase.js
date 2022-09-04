00887cab39cb372ec28e2343cf1e8e51349470da
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-05 01:34:56 +0600

    js tricky

--- true-flase.js
+++ true-flase.js
@@ -2,13 +2,16 @@
 
 // *truthy
 // any number is positive or negetive is Truthy 
-// any string including single whitespace is truthy 
-
+// any string including single whitespace is truthy '0' 
+// empty array is truthy  
+// empty object is truthy  
 
 // !falsy 
 // 0 zero is falsy value
 // empty string is falsy 
-
+// undefined
+// null 
+// Nan 
 
 
 const x = false;
