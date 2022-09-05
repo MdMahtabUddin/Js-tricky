ce8a518e7c9c04a0c3942ddf7adfd132c487bc99
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-09-06 00:48:14 +0600

    scope

---
+++ double-triple.js
@@ -0,0 +1,8 @@
+// PH 35-5 double equal (==) vs triple equal (===), implicit conversion
+
+
+// double equal check e value , not a type 
+
+// triple equal check both value & data types 
+
+// primative data type using triple equal 
---
+++ scope.js
@@ -0,0 +1,13 @@
+// PH 35-6 Block scope, global scope, Hoisting
+
+// function add(fist, second){
+// const result = fist +second;
+// console.log(result)
+// return;
+    
+// }
+// const number = add(13, 27);
+
+// let & const is block scope 
+
+// when use a var this time will be hoisting 
